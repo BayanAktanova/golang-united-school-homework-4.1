8c794e191c41b79d43320dc29e39eaa802b93203
Баян Актанова <aktanova_b@bi.group>
2022-07-05 14:02:46 +0600

    commit

--- reverse_string.go
+++ reverse_string.go
@@ -1,7 +1,7 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	byte_str := []rune(str)
+	byte_str := []rune(input)
    	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
     	byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
    	}
